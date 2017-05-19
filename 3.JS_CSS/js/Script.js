a8bac9b4197c0e703b37075fc6eacc74819b2efc
Tamara Rodríguez Manzanero <tamara90decor@gmail.com>
2017-05-19 11:26:59 +0200

    Ejercicios con css

---
+++ 3.JS_CSS/js/Script.js
@@ -0,0 +1,19 @@
+		function cambiarLetra(){
+			document.getElementById("caja").style.fontFamily="Verdana";
+			// documento.busco la Id que me pides("").soloCSS.atributoCSS = "valor del atributo"
+		}
+
+		function tamanoLetra(){
+			document.getElementById("caja").style.fontSize="85px";
+		}
+
+		function colorLetra(){
+			document.getElementById("caja").style.color="blue";
+			// los colores también se pueden buscar como colores html
+		}
+
+		function reseteando(){
+			document.getElementById("caja").style.fontFamily="arial";
+			document.getElementById("caja").style.fontSize="15px";
+			document.getElementById("caja").style.color="#000";
+		}
